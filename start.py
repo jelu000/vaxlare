96f480583bdb12e9bc7378a14ba6899fc577407b
Jens Lundeqvist <jens.lundeqvist@storsjogymnasiet.se>
2023-05-09 23:39:33 +0200

    växlare omgord

--- start.py
+++ start.py
@@ -8,91 +8,29 @@ def main():
 
     pris = input("\tMata in pris på varan i kr: ")
     inbet = input("\tMata in inbetalt pelopp i kr: ")
+    #anropar växlings funktion
+    exChangeNow(int(pris), int(inbet))
 
-    exChange(int(pris), int(inbet))
-
-
-def exChange(priset, inbetalning):
+#definerar växlingsdunktion som skriver ut växling
+def exChangeNow(priset, inbetalning):
     
-    antal_mynt = 0
-    pengar_tillbaka = 0
-
-    sedlar_mynt_dictonary = {500:0, 100:0, 50:0, 20:0, 10:0, 5:0, 1:0}
-
     if priset > inbetalning:
         print("\n\tFattas Pengar! ")
-    
+
     else:
+        vaxel_tillbaka_dictonary = vaxla.get_exchange_dict(inbetalning, priset)
         
-        #500----------------------------------------
-        antal_mynt = vaxla.exchange500(inbetalning)
-        
-        if  antal_mynt > 0:
-            sedlar_mynt_dictonary[500] = antal_mynt 
-            pengar_tillbaka = (inbetalning - priset) % 500
-
-            #print("Pengar tillbaka = " + str(pengar_tillbaka))
-        
-        #100----------------------------------------
-        antal_mynt = vaxla.exchange100(pengar_tillbaka)
-        if  antal_mynt > 0:
-            sedlar_mynt_dictonary[100] = antal_mynt 
-            pengar_tillbaka = pengar_tillbaka % 100
-
-            #print("Pengar tillbaka = " + str(pengar_tillbaka))
-        
-        #50----------------------------------------
-        antal_mynt = vaxla.exchange50(pengar_tillbaka)
-        if  antal_mynt > 0:
-            sedlar_mynt_dictonary[50] = antal_mynt 
-            pengar_tillbaka = pengar_tillbaka % 50
-
-        #print("Pengar tillbaka = " + str(pengar_tillbaka))
-        
-        #20----------------------------------------
-        antal_mynt = vaxla.exchange20(pengar_tillbaka)
-        if  antal_mynt > 0:
-            sedlar_mynt_dictonary[20] = antal_mynt 
-            pengar_tillbaka = pengar_tillbaka % 20
-
-        #print("Pengar tillbaka = " + str(pengar_tillbaka))
-
-        #10----------------------------------------
-        antal_mynt = vaxla.exchange10(pengar_tillbaka)
-        if  antal_mynt > 0:
-            sedlar_mynt_dictonary[10] = antal_mynt 
-            pengar_tillbaka = pengar_tillbaka % 10
-
-        #print("Pengar tillbaka = " + str(pengar_tillbaka))
-
-        #5----------------------------------------
-        antal_mynt = vaxla.exchange5(pengar_tillbaka)
-        if  antal_mynt > 0:
-            sedlar_mynt_dictonary[5] = antal_mynt 
-            pengar_tillbaka = pengar_tillbaka % 5
-
-        #print("Pengar tillbaka = " + str(pengar_tillbaka))
-
-        #1----------------------------------------
-        antal_mynt = vaxla.exchange1(pengar_tillbaka)
-        if  antal_mynt > 0:
-            sedlar_mynt_dictonary[1] = antal_mynt 
-            pengar_tillbaka = pengar_tillbaka % 1
-
-        #print("Pengar tillbaka = " + str(pengar_tillbaka))   
-
         print("\n\t----------------------------------------")
-        print("\tVäxel tillbaka!\n")
-        print("\tAntal 500 lappar: " + str(sedlar_mynt_dictonary[500]))
-        print("\tAntal 100 lappar: " + str(sedlar_mynt_dictonary[100]))
-        print("\tAntal 50 lappar: " + str(sedlar_mynt_dictonary[50]))
-        print("\tAntal 20 lappar: " + str(sedlar_mynt_dictonary[20]))
-        print("\tAntal 10 kronor: " + str(sedlar_mynt_dictonary[10]))
-        print("\tAntal 5 kronor: " + str(sedlar_mynt_dictonary[5]))
-        print("\tAntal 1 kronor: " + str(sedlar_mynt_dictonary[1]) + "\n\n")
+        print("\tVäxel tillbaka:\n")
+        print("\tAntal 500 lappar: " + str(vaxel_tillbaka_dictonary[500]))
+        print("\tAntal 100 lappar: " + str(vaxel_tillbaka_dictonary[100]))
+        print("\tAntal 50 lappar: " + str(vaxel_tillbaka_dictonary[50]))
+        print("\tAntal 20 lappar: " + str(vaxel_tillbaka_dictonary[20]))
+        print("\tAntal 10 kronor: " + str(vaxel_tillbaka_dictonary[10]))
+        print("\tAntal 5 kronor: " + str(vaxel_tillbaka_dictonary[5]))
+        print("\tAntal 1 kronor: " + str(vaxel_tillbaka_dictonary[1]) + "\n\n")
+
+        print("test = " + str(vaxel_tillbaka_dictonary))
 
 
-        #print("test = " + str(sedlar_mynt_dictonary))
-    
-    
 main()
--- vaxla.py
+++ vaxla.py
@@ -1,22 +1,55 @@
 
-def  exchange500(belopp):
-    return int(belopp) // 500 
+#exhange
+#in: belopp som ska växlas, valör på sedel eller mynt
+#out: antal seda-mynt man får ut i valör
+def exchange(belopp, sedel):
+    return int(belopp) // sedel
 
-def  exchange100(belopp):
-    return int(belopp) // 100
+#get_exchange_dict
+#in: inbetalt belopp, priset på varan
+#out: dictonary inhåller antal mynt-sedlar i varje valör
+def get_exchange_dict(inbetalning, priset):
+        
+    antal_mynt = 0
+    pengar_tillbaka = 0
 
-def  exchange50(belopp):
-    return int(belopp) // 50
+    sedlar_mynt_dictonary = {500:0, 100:0, 50:0, 20:0, 10:0, 5:0, 1:0}
 
-def  exchange20(belopp):
-    return int(belopp) // 20
+    #500----------------------------------------
+    pengar_tillbaka = inbetalning - priset
+    antal_mynt = exchange(pengar_tillbaka, 500)
+    pengar_tillbaka = pengar_tillbaka % 500
+    sedlar_mynt_dictonary[500] = antal_mynt 
+    #print("Pengar tillbaka = " + str(pengar_tillbaka))
+        
+    #100----------------------------------------
+    antal_mynt = exchange(pengar_tillbaka, 100)
+    pengar_tillbaka = pengar_tillbaka % 100
+    sedlar_mynt_dictonary[100] = antal_mynt 
+                
+    #50----------------------------------------
+    antal_mynt = exchange(pengar_tillbaka, 50)
+    pengar_tillbaka = pengar_tillbaka % 50
+    sedlar_mynt_dictonary[50] = antal_mynt 
+                
+    #20----------------------------------------
+    antal_mynt = exchange(pengar_tillbaka, 20)
+    pengar_tillbaka = pengar_tillbaka % 20
+    sedlar_mynt_dictonary[20] = antal_mynt 
+        
+    #10----------------------------------------
+    antal_mynt = exchange(pengar_tillbaka, 10)
+    pengar_tillbaka = pengar_tillbaka % 10
+    sedlar_mynt_dictonary[10] = antal_mynt 
+        
+    #5----------------------------------------
+    antal_mynt = exchange(pengar_tillbaka, 5)
+    pengar_tillbaka = pengar_tillbaka % 5
+    sedlar_mynt_dictonary[5] = antal_mynt 
+        
+    #1----------------------------------------
+    antal_mynt = exchange(pengar_tillbaka, 1)
+    pengar_tillbaka = pengar_tillbaka % 1
+    sedlar_mynt_dictonary[1] = antal_mynt
 
-def  exchange10(belopp):
-    return int(belopp) // 10
-
-def  exchange5(belopp):
-    return int(belopp) // 5
-
-def  exchange1(belopp):
-    return int(belopp) // 1
-
+    return sedlar_mynt_dictonary
